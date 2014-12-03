66c7c626fa0c693b06b6e1521914fa8dd9b9ad6b
gsyarov <gsyarov@gmail.com>
2014-12-03 23:30:13 +0200

    make it better

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
   MONGO_URI: process.env.MONGO_URI || 'mongodb://test:qwerty@ds053390.mongolab.com:53390/tvshowtest',
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'Facebook App Secret',
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '5f9c8c45813b1dc7c15a906818e9e246',
   FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret',
   GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'TQrG0XX0nEpAVxUycgIzyR2f',
   GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',

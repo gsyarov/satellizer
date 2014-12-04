73beb68c3911b0058f3c13deb136fd6919d889e3
gsyarov <gsyarov@gmail.com>
2014-12-04 15:38:21 +0200

    make it better

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 
 module.exports = app;

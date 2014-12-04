920fb692f523df5e3083278257350556cffd5197
gsyarov <gsyarov@gmail.com>
2014-12-04 14:13:59 +0200

    make it better

--- client/app.js
+++ client/app.js
@@ -35,39 +35,110 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ui.router', 'mgcrea.ngStra
 
     $urlRouterProvider.otherwise('/');
 
-    $authProvider.facebook({
-      clientId: '1583681655183460'
-    });
+    $authProvider.loginOnSignup = true;
+$authProvider.loginRedirect = '/';
+$authProvider.logoutRedirect = '/';
+$authProvider.signupRedirect = '/login';
+$authProvider.loginUrl = '/auth/login';
+$authProvider.signupUrl = '/auth/signup';
+$authProvider.loginRoute = '/login';
+$authProvider.signupRoute = '/signup';
+$authProvider.tokenName = 'token';
+$authProvider.tokenPrefix = 'satellizer'; // Local Storage name prefix
+$authProvider.unlinkUrl = '/auth/unlink/';
+$authProvider.authHeader = 'Authorization';
 
-    $authProvider.google({
-      clientId: '481814970662-410kc4rr87n4ace8079cu05kubmr57he.apps.googleusercontent.com'
-    });
+// Facebook
+$authProvider.facebook({
+  url: '/auth/facebook',
+  authorizationEndpoint: 'https://www.facebook.com/dialog/oauth',
+  redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host + '/',
+  scope: 'email',
+  scopeDelimiter: ',',
+  requiredUrlParams: ['display', 'scope'],
+  display: 'popup',
+  type: '2.0',
+  popupOptions: { width: 481, height: 269 }
+});
 
-    $authProvider.github({
-      clientId: '0ba2600b1dbdb756688b'
-    });
+// Google
+$authProvider.google({
+  url: '/auth/google',
+  authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+  redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+  scope: ['profile', 'email'];
+  scopePrefix: 'openid';
+  scopeDelimiter: ' ',
+  requiredUrlParams: ['scope'],
+  optionalUrlParams: ['display'],
+  display: 'popup',
+  type: '2.0',
+  popupOptions: { width: 452, height: 633 }
+});
 
-    $authProvider.linkedin({
-      clientId: '77cw786yignpzj'
-    });
+// LinkedIn
+$authProvider.linkedin({
+  url: '/auth/linkedin',
+  authorizationEndpoint: 'https://www.linkedin.com/uas/oauth2/authorization',
+  redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+  requiredUrlParams: ['state'],
+  scope: [],
+  scopeDelimiter: ' ',
+  state: 'STATE',
+  type: '2.0',
+  popupOptions: { width: 527, height: 582 }
+});
 
-    $authProvider.yahoo({
-      clientId: 'dj0yJmk9SDVkM2RhNWJSc2ZBJmQ9WVdrOWIzVlFRMWxzTXpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yYw--'
-    });
+// Twitter
+$authProvider.twitter({
+  url: '/auth/twitter',
+  type: '1.0',
+  popupOptions: { width: 495, height: 645 }
+});
 
-    $authProvider.twitter({
-      url: '/auth/twitter'
-    });
+// GitHub
+$authProvider.github({
+  url: '/auth/github',
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+  scope: [],
+  scopeDelimiter: ' ',
+  type: '2.0',
+  popupOptions: { width: 1020, height: 618 }
+});
 
-    $authProvider.live({
-      clientId: '000000004C12E68D'
-    });
+// Windows Live
+$authProvider.live: {
+  url: '/auth/live',
+  authorizationEndpoint: 'https://login.live.com/oauth20_authorize.srf',
+  redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
+  scope: ['wl.basic'],
+  scopeDelimiter: ' ',
+  requiredUrlParams: ['display', 'scope'],
+  display: 'popup',
+  type: '2.0',
+  popupOptions: { width: 500, height: 560 }
+}
 
-    $authProvider.oauth2({
-      name: 'foursquare',
-      url: '/auth/foursquare',
-      clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
-      redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host,
-      authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate'
-    });
-  });
+// OAuth 2.0
+$authProvider.oauth2({
+  url: null,
+  name: null,
+  scope: null,
+  scopeDelimiter: null,
+  clientId: null,
+  redirectUri: null,
+  popupOptions: null,
+  authorizationEndpoint: null,
+  requiredUrlParams: null,
+  optionalUrlParams: null,
+  defaultUrlParams: ['response_type', 'client_id', 'redirect_uri'],
+  responseType: 'code'
+});
+
+// OAuth 1.0
+$authProvider.oauth1({
+  url: null,
+  name: null,
+  popupOptions: null
+});
